186bb093ac9b7cfc053f9d5800fee95893782946
Alexander Braunreuther <alexander.braunreuther@googlemail.com>
2022-03-15 19:23:13 +0100

    fix: remove undefined params in build step

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,6 @@ export default function IndexPage({ page }) {
 }
 
 export async function getStaticProps({ locale, preview = false }) {
-  console.log({ params })
-
-  const isPersonalized = get(params, 'slug.0', '').startsWith(';')
-  console.log({ isPersonalized })
-
-  const audiences = isPersonalized
-    ? get(params, 'slug.0', '').split(';')[1].split(',')
-    : []
-
   const client = graphcmsClient(preview)
 
   const { page } = await client.request(pageQuery, {
